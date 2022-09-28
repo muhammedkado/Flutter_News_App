8fc9bed9a4dbf902b0544bc69f9a5a2b8f9c0427
muhammedkado <mehmetkado9@gmail.com>
2022-09-28 18:51:32 +0300

    add hello

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // heloo
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
